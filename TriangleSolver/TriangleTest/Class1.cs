e9be96d5d5269ba12570b99bab02abf0facef6fc
Susrutha <Sduvvi1678@conestoga.on.ca>
2024-10-11 19:08:33 -0400

    add Isosceles Triangle testcases

--- TriangleSolver/TriangleTest/Class1.cs
+++ TriangleSolver/TriangleTest/Class1.cs
@@ -27,7 +27,56 @@ namespace TriangleTest
             // Assert
             Assert.That(result, Is.EqualTo(expected));
         }
-        
+        //2
+        [Test]
+        public void ValidTriangle_Input5and5and8_OutputIsosceles()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 5;
+            int side3 = 8;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        //3
+        [Test]
+        public void ValidTriangle_Input6and6and10_OutputIsosceles()
+        {
+            // Arrange
+            int side1 = 6;
+            int side2 = 6;
+            int side3 = 10;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        //4
+        [Test]
+        public void ValidTriangle_Input9and9and7_OutputIsosceles()
+        {
+            // Arrange
+            int side1 = 9;
+            int side2 = 9;
+            int side3 = 7;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+
 
 
 
